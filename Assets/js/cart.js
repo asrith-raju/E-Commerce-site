2f57d278b8a06c907c4444715e406125e3a843e3
asrith-raju <asrithraju143@gmail.com>
2025-07-27 12:26:05 +0530

    modified cart page

--- Assets/js/cart.js
+++ Assets/js/cart.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-  const container = document.querySelector(".cart-items"); 
+  const container = document.querySelector(".cart-items");
 
   if (cart.length === 0) {
     container.innerHTML = "<p>Your cart is empty.</p>";
@@ -9,26 +9,59 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   cart.forEach(({ image, name, brand, price }) => {
+    const numericPrice = parseInt(price.toString().replace(/[^0-9]/g, ""));
+
     const div = document.createElement("div");
     div.className = "cart-item";
     div.innerHTML = `
       <img src="${image}" alt="${name}" class="cart-image">
       <div class="cart-details">
+      <div class="top">
         <p><strong>${name}</strong></p>
         <p>Brand: ${brand}</p>
-        <p>Price: ${price}</p>
+        </div>
+        <div class="bottom">
+        <div class="count">
+        <button class="minus">-</button>
+        <button class="plus">+</button>
+        <input type="text" class="increment" value="1" readonly>
+        </div>
+        <p>total:
+        <input type="text" class="priceadd" value="${numericPrice}" readonly></p>
+        </div>
         <a href="#" class="delete"><i class="fa-solid fa-trash "></i></a>
       </div>
     `;
+    let increment = div.querySelector('.increment');
+    let plus = div.querySelector('.plus')
+    let minus = div.querySelector('.minus')
+    let priceadd = div.querySelector('.priceadd')
+    plus.addEventListener("click", () => {
+      increment.value = parseInt(increment.value) + 1;
+      const quantity = parseInt(increment.value)
+      const total = parseInt(numericPrice) * quantity
+      priceadd.value = total;
+    });
+     minus.addEventListener("click", () => {
+      if (parseInt(increment.value) > 1) {
+        increment.value = parseInt(increment.value) - 1;
+        const quantity = parseInt(increment.value)
+        const total = parseInt(numericPrice) * quantity
+        priceadd.value = total;
+      }
+    });
     const removeBtn = div.querySelector(".delete")
-    removeBtn.addEventListener("click",()=>{
+    removeBtn.addEventListener("click", () => {
       let cart = JSON.parse(localStorage.getItem("cart")) || [];
-      let newcart=cart.filter(item => item.name!==name);
-      
+      let newcart = cart.filter(item => item.name !== name);
+
       localStorage.setItem("cart", JSON.stringify(newcart));
-      
+
       location.reload();
     })
     container.appendChild(div);
+    
+   
   });
 });
+
